0f3125791f9d1db4ae00cb81be3e39bf85997207
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-16 18:05:23 +0900

    delete , create test 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -40,10 +40,49 @@ describe('MoviesService', () => {
     it('should throw 404 error', () => {
       try {
         service.getOne(999);
-      } catch(e) {
+      } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual(`Movie with ID: 999 not found.`);
       }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+
+      console.log('beforeCreate:', beforeCreate);
+      console.log('afterCreate:', afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
 });
