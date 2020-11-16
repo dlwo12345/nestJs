aebc3eca3c0e0d8b24a8d6ff825dda3d029a8854
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-11-16 16:01:32 +0900

    global pipe 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true, // 데코레이터 없으면 유효성 검사 스킵
+    forbidNonWhitelisted: true, // 이상한 요소 거름
+    transform: true, // 타입 자동변환
+  }));
   await app.listen(3000);
 }
 bootstrap();
